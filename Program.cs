ac77460230c37bef2dd711d041e6c520c011b774
LeaLiv <leahl11730@gmail.com>
2025-04-23 23:42:42 +0300

    Add token service and definitions in program.cs need to use it

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
+using System.Text;
 using CoreProject.Middlewares;
 using firstProject.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(options=>{
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
     options.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
 });
 builder.Services.AddShoeConst();
@@ -12,12 +18,50 @@ builder.Services.AddUserConst();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(cfg =>
+    {
+        cfg.RequireHttpsMetadata = false;
+        cfg.TokenValidationParameters = UserTokenService.GetTokenValidationParameters();
 
+    });
+builder.Services.AddAuthorization(cfg =>
+{
+    cfg.AddPolicy("admin",
+    policy => policy.RequireClaim("type", "admin"));
+    cfg.AddPolicy("user",
+    policy => policy.RequireClaim("type", "user"));
+
+});
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(//c=>{
-    //c.SwaggerDoc("v1", new() { Title = "firstProject", Version = "v1" });}
-);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Core", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    { new OpenApiSecurityScheme
+    {
+     Reference = new OpenApiReference { 
+        Type = ReferenceType.SecurityScheme,
+         Id = "Bearer"}
+    },
+    new string[] {}
+    }
+});
+});
+//c=>{
+//c.SwaggerDoc("v1", new() { Title = "firstProject", Version = "v1" });}
+
 // builder.Services.AddSingleton<IShoesService,ShoesServiceConst>();
 var app = builder.Build();
 
@@ -28,14 +72,15 @@ if (app.Environment.IsDevelopment())
 {
     // app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core v1"));
 }
 //------------------------------------------
-app.UseMailMiddleware();
-app.UseLogMiddleware();
+// app.UseMailMiddleware();
+// app.UseLogMiddleware();
 
-app.UseMyErrorMiddleware();
+// app.UseMyErrorMiddleware();
 //-----------------------------------------
+
 
 app.UseDefaultFiles();
 
